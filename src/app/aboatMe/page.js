23721fb628f76420e3551fc383a2ea139ebe3866
elisaeidi <softwareEngineer.saeedi@gmail.com>
2024-08-17 16:46:21 +0330

    create success page

--- src/app/aboatMe/page.js
+++ src/app/aboatMe/page.js
@@ -1,26 +1,51 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Aboat() {
+    const startYear=94;
+    const endYear=98;
+    const nintynine = 99;
+    const time = 3;
     return (
-        <div className="flex-1 flex flex-col  text--cyan-950 p-4">
+        <div dir="rtl" className="flex-1 flex flex-col  text--cyan-950 p-4">
             {/* <Image className='rounded-full' width={100} height={100} src="/images/Elahe_Saeedi.jpg" /> */}
 
             {/* profile */}
             <fieldset className="border border-orange-600 p-4 m-6">
-                <legend className="italic text-base font-bold px-2 text-orange-600">profile</legend>
-                <h1>hi ,I'm Elahe Saeedi</h1>
-                <p>Elaheh Saeedi
-                    [Your Phone Number] | [Your Email Address] | [Your LinkedIn Profile] | [Your GitHub Profile] | [Your Location]</p>
+                <legend className="italic text-base font-bold px-2 text-orange-600">پروفایل</legend>
+
+                {/* <p>Elaheh Saeedi</p>
+                <p>you can conenction to Me by</p>
+                <p><span>Phone Number : </span><span className="font-light text-base">09036047690</span></p>
+                <p><span>Email Address : </span><span className="font-light text-base">eng.saeedi1993@gmail.com</span></p>
+                <p><span>GitHub Profile : </span><span className="font-light text-base">softwarEngineer.saeedi@gmail.com</span></p>
+                <p><span>Location : </span><span className="font-light text-base">Kerman,Iran</span></p> */}
+                {/* [Your LinkedIn Profile]  */}
+                <p>سلام من <strong>الهه سعیدی</strong> هستم</p>
+                {/* 
+                 */}
+                 <span>{`شروع تحصیلات من از سال ${startYear.toLocaleString("fa")}-${endYear.toLocaleString("fa")} در رشته مهندسی کامپیوتر (نرم افزار ) بود`}</span>
+                 <span>{` مدتی بعد با راهنمایی اساتیدم وارد حوزه توسعه وبسایت شدم و برای اولین بار توانستم در شرکت فرداپ کرمان به عنوان کارآموز با محیط و پروژه های واقعی آشنا بشوم  بعد از  گذشت `}</span>
+                 <span>{`مدت ${time.toLocaleString("fa")} ماه به عنوان کارشناس فرانت اند مشغول به کار شدم `}</span>
+                 <span>و در حال حاضر تصمیم به کسب تجربه های جدید با فریمورک next.js هستم </span>
+                 <span>همچنین اقدام به پیاده سازی یک فروشگاه اینترنتی برای کسب تجربه و آمادگی هرچه بیشتر برای شروع همکاری با شرکت هایی که با تکنولوژی های جدید مشغول به فعالیت هستند کرده ام </span>
+                <p>در حال حاضر مشغول پیاده سازی این فروشگاه با next.js هستم و قسمت های تکمیل شده آن در گیت هاب من می توانید مشاهده کنید</p>
+                <p className="mt-4">باعث افتخار من است که شما بزرگواران نظرات و پیشنهادات همکاری خود را از طریق راه های ارتباطی زیر با من به اشتراک بگذارید :</p>
+                <p><span>شماره تماس : </span><span dir="rtl" className="font-light text-base text-orange-600">0903-604-7690</span></p>
+                <p><span>آدرس ایمیل : </span><span className="font-light text-base text-orange-600"><Link href="eng.saeedi1993@gmail.com">eng.saeedi1993@gmail.com</Link></span></p>
+                <p><span>پروفایل گیت هاب : </span><span className="font-light text-base text-orange-600"><Link href="https://github.com/elisaeidi/digimall.git">softwarEngineer.saeedi@gmail.com</Link></span></p>
+                <p><span>محل سکونت : </span><span className="font-light text-base text-orange-600">کرمان</span></p>
+                {/* [Your LinkedIn Profile]  */}
 
             </fieldset>
             {/* Professional Summary */}
-            <fieldset className="border border-orange-600 p-4 m-6">
+            {/* <fieldset className="border border-orange-600 p-4 m-6">
                 <legend className="italic text-base font-bold px-2 text-orange-600">Professional Summary</legend>
                 <p> Dedicated Front-End Developer with 4 years of experience in website development, specializing in React and Next.js frameworks. Proficient in creating dynamic and responsive web applications with a strong focus on user experience. Currently enhancing skills in Next.js and working on a personal e-commerce project to deepen understanding of advanced web technologies.</p>
-            </fieldset>
+            </fieldset> */}
             {/* Technical Skills  */}
             <fieldset className="border border-orange-600 p-4 m-6">
-                <legend className="italic text-base font-bold px-2 text-orange-600">Technical Skills </legend>
+                <legend className="italic text-base font-bold px-2 text-orange-600">مهارت ها</legend>
                 <ul className="list-none p-2">
                     <li className="font-bold">Languages
                         <ul className="list-disc font-light text-base p-4">
@@ -50,7 +75,7 @@ export default function Aboat() {
                 </ul>
             </fieldset>
             {/* Professional Experience */}
-            <fieldset className="border border-orange-600 p-4 m-6">
+            {/* <fieldset className="border border-orange-600 p-4 m-6">
                 <legend className="italic text-base font-bold px-2 text-orange-600">Professional Experience</legend>
                 <p>
                     Professional Experience
@@ -64,7 +89,7 @@ export default function Aboat() {
                     Conducted code reviews and provided mentorship to junior developers.
                 </p>
 
-            </fieldset>
+            </fieldset> */}
         </div>
     )
 
--- src/app/success/page.js
+++ src/app/success/page.js
@@ -1,5 +1,17 @@
-export default function Success(){
+import Link from "next/link";
+
+export default function Success() {
     return (
-        <div>success</div>
+        <div className="flex-1 flex flex-row justify-center items-center">
+            <div className=" border border-blue-500 rounded-lg p-20">
+                <div>
+                    <p className="text-green-500 font-bold text-base">پرداخت با موفقیت اتجام شد </p>
+                    <p className="text-gray-400 mt-4 text-sm"><span>کد رهگیری : </span> <span>123456789</span></p>
+                </div>
+                <button className="bg-blue-500 text-white border border-blue-500 rounded-lg px-4 mt-4 flex flex-row justify-end">
+                    <Link href="/home">تایید</Link>
+                </button>
+            </div>
+        </div>
     )
 }
