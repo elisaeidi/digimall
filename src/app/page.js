96d1e983f0e0adb1e26bdf774eace6753d971c73
elisaeidi <softwareEngineer.saeedi@gmail.com>
2024-08-29 11:55:44 +0330

    changes files

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,17 @@
-import Image from 'next/image'
+
 
 export default function App() {
+    props.flag = true
+
     return (
-        <div dir='rtl' className='flex flex-row justify-center items-center h-screen bg-cover bg-center bg-custom-image mobile:min-w-full'>
-       
-       {/* <h1 className='text-cyan-950 mt-8 '>DIGI MALL</h1> */}
-       
-        {/* <form>
-            <fieldset className='bg-white  border rounded-lg p-4 flex flex-col '>
-                <legend className='mb-2'>ورود</legend>
-                <input type='number' name='mobile' className='border border-blue-300 m-4 rounded-lg py-2 px-4'/>
-                <input type='password' name='password' className='border border-blue-300 m-4 rounded-lg py-2 px-4'/>
-            </fieldset>
-        </form> */}
-        </div> 
+        <div className='flex flex-row justify-center items-center h-screen bg-cover bg-center bg-custom-image mobile:min-w-full'>
+            <div className=" flex flex-col justify-center items-center bg-cyan-950 border border-cyan-950 rounded-lg px-8 py-4">
+                <label className="text-violet-500">به دیجی مال خوش آمدید دوست داری از کجا شروع کنی؟</label>
+                <div className="min-w-full flex flex-row justify-around p-4">
+                    <button className="text-white border border-green-600 rounded-lg p-2 mt-16 mx-4 bg-green-600">می خوام عضو بشم</button>
+                    <button className="text-white border border-blue-600 rounded-lg p-2 mt-16 mx-4 bg-blue-600">می خوام محصولات رو ببینم</button>
+                </div>
+            </div>
+        </div>
     )
 }
